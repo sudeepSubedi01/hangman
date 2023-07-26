8ff37842866584d63893beac6c5845aa4429e828
Sudip Subedi <sudip.subedi99@gmail.com>
2023-07-26 09:27:20 +0545

    Reading input

--- hangman.c
+++ hangman.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
+void clear_stdin() {
+  int c;
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+}
+
+int is_alphabet(char ch) {
+  return ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'));
+}
+
+char get_input() {
+  char ch = 0;
+  printf("Enter your guess: ");
+  scanf("%c", &ch);
+  clear_stdin();
+
+  while(!is_alphabet(ch)) {
+    printf("Invalid input. Enter again: ");
+    scanf("%c", &ch);
+    clear_stdin();
+  }
+  
+  return ch;
+}
+
 int main() {
-  printf("Hello World!\n");
+  char guess = get_input();
+  printf("Your guess is: %c\n", guess);
 
   return 0;
 }
