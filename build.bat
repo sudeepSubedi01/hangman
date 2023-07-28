9c72083c71d9f080ebd92d4fa2c0ade631b85a8a
Sudip Subedi <sudip.subedi99@gmail.com>
2023-07-28 09:09:49 +0545

    windows-build-support

--- build.bat
+++ build.bat
@@ -1,4 +1,13 @@
 @echo off
 
+where gcc >nul 2>nul
+if %ERRORLEVEL% neq 0 goto SkipBuild
+
 if not exist "build" mkdir build
 gcc -o build/hangman.exe hangman.c
+goto Finished
+
+:SkipBuild
+echo "gcc not found"
+
+:Finished
